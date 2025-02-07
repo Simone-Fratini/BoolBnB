f8184ac2c0ca6f123bab27c38ee5e88b0a677c71
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-07 18:23:20 +0100

    prova header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <header className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-center p-4">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <>
+            <header className="text-white text-center p-4 bg-red-300 flex justify-between fixed w-screen">
+                <div>icona</div>
+                <div></div>
+                <div>login</div>
+            </header>
+            <div className="flex flex-col gap-4 justify-center items-center h-[60vh] bg-red-200">
+              <div className="text-center">
+                <h1 className="text-3xl">Title casual</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, corporis!</p>
+              </div>
+                <input
+                    type="text"
+                    className="border bg-[#fcfcfc] border-slate-400 px-4 py-2 rounded-lg sticky top-[10px]"
+                    placeholder="ciao"
+                />
+            </div>
+        </>
+    );
 };
 
 export default Header;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Main({ children }) {
-    return <main>{children}</main>;
+    return <main className="min-h-[120vh] sticky top-[10vh]">{children}</main>;
 }
 
 export default Main;
