5f8be5850ad537a83c2b022aba0c601511b115ea
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-07 17:48:27 +0100

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./pages/DefaultLayout";
+import HomePage from "./pages/Homepage";
+import SearchPropertyPage from "./pages/SearchPropertyPage";
+import AddPropertyPage from "./pages/AddPropertyPage";
+import PropertyDetail from "./pages/PropertyDetailPage";
 function App() {
-  return (
-    <>
-      <Header />
-      <p className="text-3xl font-bold underline text-pink-600">Tailwind</p>
-      <Footer />
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" Component={DefaultLayout}>
+                    <Route index Component={HomePage} />
+                    <Route path="search" Component={SearchPropertyPage} />
+                    <Route path="addproperty" Component={AddPropertyPage} />
+                    <Route path="detail/:id" Component={PropertyDetail} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/AddPropertyPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddPropertyPage() {
+    return <div>AddProperty</div>;
+}
+
+export default AddPropertyPage;
