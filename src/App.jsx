a59e0392e8d860c13178ec446cbcb46839f9f31b
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-08 16:24:27 +0100

    aggiunta globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./pages/DefaultLayout";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import SearchPropertyPage from "./pages/SearchPropertyPage";
 import AddPropertyPage from "./pages/AddPropertyPage";
 import PropertyDetail from "./pages/PropertyDetailPage";
 import { GlobalProvider } from "./Context/GlobalContext";
 function App() {
-  return (
-    <GlobalProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={DefaultLayout}>
-            <Route index Component={HomePage} />
-            <Route path="search" Component={SearchPropertyPage} />
-            <Route path="addproperty" Component={AddPropertyPage} />
-            <Route path="detail/:id" Component={PropertyDetail} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </GlobalProvider>
-  );
+    return (
+        <GlobalProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" Component={DefaultLayout}>
+                        <Route index Component={HomePage} />
+                        <Route path="search" Component={SearchPropertyPage} />
+                        <Route path="addproperty" Component={AddPropertyPage} />
+                        <Route path="detail/:id" Component={PropertyDetail} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </GlobalProvider>
+    );
 }
 
 export default App;
---
+++ src/globals/apiUrls.jsx
@@ -0,0 +1,4 @@
+export const baseUrl = "http://localhost/3000"
+export const propsEndpoint = "/properties"
+export const revsEndpoint = "/reviews"
+export const imagesUrl = "http://localhost/3000/images";
