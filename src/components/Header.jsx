22c113a1282813800969d0ed45b49030de2dda4e
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-08 08:36:09 +0100

    aggiunta mockups

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,36 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <>
-            <header className="text-white text-center p-4 bg-red-300 flex justify-between fixed w-screen">
+            <header className="bg-white text-center p-4 flex justify-between fixed top-0 w-screen z-20 shadow-lg shadow-slate-400">
                 <div>icona</div>
                 <div></div>
                 <div>login</div>
             </header>
-            <div className="flex flex-col gap-4 justify-center items-center h-[60vh] bg-red-200">
-              <div className="text-center">
-                <h1 className="text-3xl">Title casual</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, corporis!</p>
-              </div>
-                <input
-                    type="text"
-                    className="border bg-[#fcfcfc] border-slate-400 px-4 py-2 rounded-lg sticky top-[10px]"
-                    placeholder="ciao"
-                />
+            <div
+                style={{
+                    backgroundImage: "url('/images/hero_image.jpg')",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                }}
+                className="flex flex-col gap-4 justify-center items-center h-[60vh]  text-white sticky mt-[7vh]"
+            >
+                {/* overlay */}
+                <div className="absolute bg-black/50 inset-0 -z-10"></div>
+
+                <div className="text-center">
+                    <h1 className="text-3xl">Title casual</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Dolor, corporis!
+                    </p>
+                </div>
+                <div className="relative z-30">
+                  <input
+                      type="text"
+                      className="border bg-[#fcfcfc] border-slate-400 px-4 py-2 rounded-lg relative z-30"
+                      placeholder="ciao"
+                  />
+                </div>
             </div>
         </>
     );
