dd94f288d5ef74e94a41cbd6a0eed769e6b55c52
DoriaFabio <doriafabio2001@gmail.com>
2025-02-10 13:38:42 +0100

    Adjust searchbar and add images section "where"

--- src/pages/SearchPropertyPage.jsx
+++ src/pages/SearchPropertyPage.jsx
@@ -22,44 +22,62 @@ function SearchPropertyPage() {
 
 
   return (
-    <div className='flex flex-col md:flex-row mt-5 mx-10 bg-linear-90/oklch  from-[#d4c685] to-[#a7d3a6] justify-around gap-x-8 gap-y-4 px-5'>
+    <form className='flex flex-col md:flex-row mt-5 mx-10 bg-linear-90/oklch  from-[#d4c685] to-[#a7d3a6] justify-around gap-x-8 gap-y-4 px-5 py-4'>
       {/* Place section */}
-      <div className='my-5 '>
-        <select name="place" id="place" className='bg-amber-100 h-14 p-3 w-full'>
+      <div>
+        <label htmlFor="place">Dove</label>
+        <select name="place" id="place" className='bg-amber-100 h-12 p-3 w-full'>
           <option value="all">Sono flessibile</option>
           <option value="italy">Italia</option>
           <option value="europe">Europa</option>
           <option value="america">America</option>
         </select>
       </div>
-      {/* date checkIn section */}
-      <div className='my-5 grid grid-cols-2 gap-4'>
-        <input
-          type="date"
-          id="start-date"
-          ref={CheckIn}
-          onChange={handleStartDateChange}
-          placeholder='Check-In'
-          className='bg-amber-100 h-14 p-3 w-full'
-        />
+      {/* date section */}
+      <div className='grid grid-cols-2 gap-4'>
+        {/* date checkIn section */}
+        <div>
+          <label htmlFor="start-date">Check-In</label>
+          <input
+            type="date"
+            id="start-date"
+            ref={CheckIn}
+            onChange={handleStartDateChange}
+            placeholder='Check-In'
+            className='bg-amber-100 h-12 p-3 w-full'
+          />
+        </div>
         {/* date checkOut section */}
-        <input
-          type="date"
-          id="end-date"
-          ref={CheckOut}
-          placeholder='Check-Out'
-          className='bg-amber-100 h-14 p-3 w-full'
-        />
+        <div>
+          <label htmlFor="end-date">Check-Out</label>
+          <input
+            type="date"
+            id="end-date"
+            ref={CheckOut}
+            placeholder='Check-Out'
+            className='bg-amber-100 h-12 p-3 w-full'
+          />
+        </div>
       </div>
-      {/* Children Section */}
-      <div className='my-5 md:grid md:grid-cols-3 gap-4 flex flex-col'>
-        <input type="number" name="children" id="children" min={0} placeholder='Bambini' className='bg-amber-100 h-14 p-3 w-full' />
+      {/* # people Section */}
+      <div className='md:grid md:grid-cols-3 gap-4 flex flex-col'>
+        {/* children section */}
+        <div>
+          <label htmlFor="children">Bambini</label>
+          <input type="number" name="children" id="children" min={0} className='bg-amber-100 h-12 p-3 w-full' />
+        </div>
         {/* Adult Section */}
-        <input type="number" name="adult" id="adult" min={0} placeholder='Adulti' className='bg-amber-100 h-14 p-3 w-full' />
+        <div>
+          <label htmlFor="adult">Adulti</label>
+          <input type="number" name="adult" id="adult" min={0}  className='bg-amber-100 h-12 p-3 w-full' />
+        </div>
         {/* Animal section */}
-        <input type="number" name="animal" id="animal" min={0} placeholder='Animali' className='bg-amber-100 h-14 p-3 w-full' />
+        <div>
+          <label htmlFor="animal">Animali</label>
+          <input type="number" name="animal" id="animal" min={0} className='bg-amber-100 h-12 p-3 w-full' />
+        </div>
       </div>
-    </div>
+    </form>
   )
 }
 
